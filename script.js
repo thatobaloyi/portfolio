3dfafaab7a7660603416b3ebb7b5e4375a3dfad0
Thato Baloyi <thato2313321@gmail.com>
2023-08-20 19:47:29 +0200

    files added

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let scrollpos = localStorage.getItem('scrollpos');
+    if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function(e) {
+    localStorage.setItem('scrollpos', window.scrollY);
+};
